0f41e2c8ee936b4588847366cf98ec0c0a08aea3
Hasnat Ali <hasnat.ali@weatherwalay.com>
2026-05-17 03:31:18 +0500

    already  sub change

--- src/jobs/processorCron.js
+++ src/jobs/processorCron.js
@@ -32,7 +32,7 @@ async function fetchProcessorCount() {
 async function startProcessorCron() {
   // await fetchProcessorCount();
   // run at minute 0 of every hour
-  cron.schedule('29 * * * *', async () => {
+  cron.schedule('32 * * * *', async () => {
     console.log(new Date().toISOString(), 'Running hourly processor cron job');
     await fetchProcessorCount();
   });
