b398a540fef9ed590e6a74511b805e6cd91aaa5f
Hasnat Ali <hasnat.ali@weatherwalay.com>
2026-05-17 03:35:35 +0500

    time change

--- src/jobs/processorCron.js
+++ src/jobs/processorCron.js
@@ -32,7 +32,7 @@ async function fetchProcessorCount() {
 async function startProcessorCron() {
   // await fetchProcessorCount();
   // run at minute 0 of every hour
-  cron.schedule('34 * * * *', async () => {
+  cron.schedule('36 * * * *', async () => {
     console.log(new Date().toISOString(), 'Running hourly processor cron job');
     await fetchProcessorCount();
   });
