91dec8e7311684d294b791462850bf44210cd061
Hasnat Ali <hasnat.ali@weatherwalay.com>
2026-05-17 01:52:47 +0500

    tracked

--- src/jobs/processorCron.js
+++ src/jobs/processorCron.js
@@ -1,22 +1,29 @@
 const cron = require('node-cron');
 const db = require('../db');
+const { checkZongNum } = require('../utils/zong/clientele');
 
 async function fetchProcessorCount() {
   try {
-    const query = `SELECT *  FROM processor
+    const query = `SELECT * FROM processor
       WHERE status = 0
         AND created_at >= NOW() - INTERVAL 1 HOUR`;
     const [rows] = await db.execute(query);
-    const count = rows[0]?.count ?? 0;
+    const count = rows.length;
 
-
-    console.log("rows", rows);
+    console.log('rows', rows);
     console.log(new Date().toISOString(), 'Processor numbers last hour with status 0:', count);
+
+    for (const row of rows) {
+      console.log(`ID: ${row.id}, Cellno: ${row.msisn}, Status: ${row.status}, Created At: ${row.created_at}`);
+      await processProcessorRow(row);
+    }
   } catch (error) {
     console.error('Error fetching processor count:', error);
   }
 }
-
+const processProcessorRow = async (row) => {
+  checkZongNum({ cellno: row.msisn, subDomain: row.sub_domain })
+}
 async function startProcessorCron() {
   await fetchProcessorCount();
   // run at minute 0 of every hour
